e65a96a8a2ce267c06c8436eab4854e2d4fe630e
Shawn <bartandfosto@gmail.com>
2016-07-22 16:14:20 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
 
   resources :users, except: [:new, :index]
-  resources :articles
+  resources :articles, only: [:new, :create]
 end
