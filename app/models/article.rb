c7f76620498e65ecbfd53959eed2c2142c364fd4
Brian Lee <email": null>
2016-07-22 12:23:26 -0400

    Undo changes to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,27 +8,4 @@ class Article < ApplicationRecord
 
   after_save :url_check
 
-  CONTENT_TAG = { "medium.com": ".postArticle-content",
-                "espn.com": ".article",
-                "npr.org": ".story"
-                }
-
-  def url_check
-    @domain = domain(self.url)
-    if @domain == "medium.com"
-      self.url.gsub!(/#.*\z/, '')
-    end
-  end
-
-  def domain(url)
-    uri = URI.parse(url)
-    domain = uri.host
-  end
-
-  def text
-    tag = CONTENT_TAG[domain]
-    doc = Nokogiri::HTML(open(url))
-    @text = doc.css(tag).text
-  end
-
 end
