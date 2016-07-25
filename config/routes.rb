fbc566cc092372c7b2877487a5c1f2e265e1d598
caitlinchai <caitlinchai.2012@gmail.com>
2016-07-25 15:05:22 -0400

    style users homepage

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :index] 
   resources :articles, only: [:new, :create, :show]
   resources :audios
+  resources :user_articles, only: [:destroy]
 
 end
