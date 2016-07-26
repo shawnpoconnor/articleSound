1a7ba4c4051cc937c91d34fef5b2410e90d34640
caitlinchai <caitlinchai.2012@gmail.com>
2016-07-26 19:12:21 -0400

    styling for home pg and users profile. change pallet

--- app/controllers/user_articles_controller.rb
+++ app/controllers/user_articles_controller.rb
@@ -13,10 +13,8 @@ class UserArticlesController < ApplicationController
   def create
     @user_article = UserArticle.new(article_id: params[:user_article][:article_id], user_id: current_user.id)
     if @user_article.save
-      flash[:notice]="added"
       redirect_to root_url
     else
-      flash[:notice]="already in your thing"
       redirect_to root_url
     end
   end
