d54401f8ff5a39ec5d7f86753ae27c2e64431b04
caitlinchai <caitlinchai.2012@gmail.com>
2016-07-26 09:54:37 -0400

    split recently added into history and queue columns

--- app/controllers/user_articles_controller.rb
+++ app/controllers/user_articles_controller.rb
@@ -6,8 +6,7 @@ class UserArticlesController < ApplicationController
   end
 
   def destroy
-    article = Article.find(params[:id])
-    UserArticle.find_by(article_id: article.id, user_id:current_user.id).destroy
+    UserArticle.find(params[:id]).destroy
     redirect_to current_user
   end
 end
