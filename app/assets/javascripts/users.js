f3422307a847ca742d8d9ebcb85cbc17c68590f6
Shawn <email": null>
2016-07-25 21:24:03 -0400

    adds js to deal with switching buttons and play pause

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,9 +5,18 @@ $(document).ready(function() {
   Materialize.updateTextFields();
 
   $('.play').click(function(e){
+    e.preventDefault();
+    var pause = this.parentElement.nextElementSibling;
     var url = $(this).data("url");
-    $('#player').prop('src', url);
-  
+
+    if($(this).hasClass('clicked')) {
+      $('#player').get(0).play();
+    }else {
+      $('#player').prop('src', url);
+    }
+    $(pause).removeClass("inactive");
+    $(this.parentElement).addClass("inactive");
+
     $('#player').on('ended', function(){
       var id = $(this).data("id");
       $.ajax({
@@ -17,7 +26,12 @@ $(document).ready(function() {
     }.bind(this));
   });
 
-
+  $('.pause').click(function(e){
+    var pause = this.parentElement.previousElementSibling;
+    $('#player').get(0).pause();
+    $(pause).removeClass("inactive");
+    $(this.parentElement).addClass("inactive");
+  });
 
   $('.article').on('click', '.show-text', function(e) {
     var text = this.parentElement.lastChild.previousElementSibling;
