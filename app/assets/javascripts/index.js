2ecec44c2e39789346126b135600812af7c910f2
Brian Lee <email": null>
2016-07-28 10:44:45 -0400

    DRY index.js code

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -12,21 +12,6 @@ $(document).ready(function() {
       data:$(this).serialize()
     }).done(function(response){
       $('#trending').html(response);
-    }).fail(function(response){
-      console.log(response);
-    });
-  });
-
-  $('#trending').on('submit','.edit_user_article',function(e){
-    e.preventDefault();
-    $.ajax({
-      url: $(this).attr('action'),
-      method:$(this).attr('method'),
-      data:$(this).serialize()
-    }).done(function(response){
-      $('#trending').html(response);
-    }).fail(function(response){
-      console.log(response);
     });
   });
 });
